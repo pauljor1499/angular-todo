4f07e42ac4dfb5ef5bd0781757d5ceeed572032b
paulj <jorpaulvincent@gmail.com>
2024-02-28 19:56:52 +0800

    display total tasks

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -13,14 +13,27 @@ export class DashboardComponent {
 
 
 	tasks: Task[] = [];
+	newTodo: string = '';
 
 	ngOnInit(): void {
 		//Called after the constructor, initializing input properties, and the first call to ngOnChanges.
 		//Add 'implements OnInit' to the class.
+		this.getAllTasks()
 	}
 
 	getAllTasks(): void {
 		this.tasks = this.taskListService.getAllTasks();
+	}
+
+	addTodo() {
+		// if (this.newTodo.trim() !== '') {
+		// 	this.todos.push(this.newTodo);
+		// 	this.newTodo = ''; // Clear input after adding todo
+		// }
+	}
+
+	removeTodo(index: number) {
+		// this.todos.splice(index, 1);
 	}
 
 }
