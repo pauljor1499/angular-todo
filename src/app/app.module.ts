7012a080622d2f0bf8fe84057af80ec3be46ca01
paulj <jorpaulvincent@gmail.com>
2024-02-29 12:54:06 +0800

    create table and modal components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms'; //for input
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { ModalComponent } from './component/commons/modal/modal.component';
+import { TableComponent } from './component/commons/table/table.component';
 
 @NgModule({
-	declarations: [AppComponent, DashboardComponent,],
+	declarations: [AppComponent, DashboardComponent, ModalComponent, TableComponent,],
 	imports: [BrowserModule, AppRoutingModule, FormsModule,],
 	providers: [],
 	bootstrap: [AppComponent],
