89b69054db4061f8bcd4996c7e63654456e7c679
paulj <jorpaulvincent@gmail.com>
2024-03-02 23:35:45 +0800

    create router directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './router/app-routing.module';
 import { FormsModule } from '@angular/forms'; //for input
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
